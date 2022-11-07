37c3c3ff05dde2357eed47701ccf1f0542d686b7
satyan0 <113288362+satyan0@users.noreply.github.com>
2022-11-07 23:58:48 +0530

    Add files via upload

---
+++ test_case_generator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    while(1)
+    {
+        int random = (unsigned)(rand() % 1000);
+        printf("%d\n",random);
+        
+    }
+    
+}
